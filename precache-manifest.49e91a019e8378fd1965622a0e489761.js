378de3f47dd6e9fc3531ee39bd9f4afd2b61baed
alexei.zamyatin <alexei.zamyatin@gmail.com>
2020-10-02 21:48:12 +0200

    Updates

---
+++ precache-manifest.49e91a019e8378fd1965622a0e489761.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f184061fffe6b995adcc64399bcb61f",
+    "url": "/polkabtc.io/index.html"
+  },
+  {
+    "revision": "50a41af03a7e26bbb1d2",
+    "url": "/polkabtc.io/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "16914f7fd90e99f2bedd",
+    "url": "/polkabtc.io/static/css/main.bdd3cb36.chunk.css"
+  },
+  {
+    "revision": "50a41af03a7e26bbb1d2",
+    "url": "/polkabtc.io/static/js/2.ac01b8d3.chunk.js"
+  },
+  {
+    "revision": "046b48f56e27641ebc2251c95cd730c6",
+    "url": "/polkabtc.io/static/js/2.ac01b8d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16914f7fd90e99f2bedd",
+    "url": "/polkabtc.io/static/js/main.02d9ea8d.chunk.js"
+  },
+  {
+    "revision": "b35e340e39beb05de07b",
+    "url": "/polkabtc.io/static/js/runtime-main.fdcf2853.js"
+  },
+  {
+    "revision": "6897aab9cff317c4609ad6db96a0eeaa",
+    "url": "/polkabtc.io/static/media/PolkaBTC_white.6897aab9.svg"
+  },
+  {
+    "revision": "1b810141565ab7983b2f8ac6ed43fb6f",
+    "url": "/polkabtc.io/static/media/background.1b810141.png"
+  },
+  {
+    "revision": "f20a96080457fa15638eeb45d23f35f0",
+    "url": "/polkabtc.io/static/media/web3foundation_grants_badge_black.f20a9608.png"
+  }
+]);
